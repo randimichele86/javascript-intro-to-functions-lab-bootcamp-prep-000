8fa799b3212cd64e6280a14c3c9c99ea4a06c524
Randi Fisher <randimichele86@gmail.com>
2017-09-20 13:35:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+if logShout(string) {
+  console.log("Yes, Indeed!")
+}
+if logWhisper(string) {
+  console.log("I cant hear you")
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+}
